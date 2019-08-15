99466b0446adec2c79f95cba5136ef07885779df
gforti <gforti@its.jnj.com>
2019-08-15 09:56:16 -0400

    updates to title

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports.title = 'You think you know'
-module.exports.subtitle = 'UI'
+module.exports.subtitle = 'UI Gameshow'
 module.exports.AUTH = {login: 'host', password: '1234'}
 module.exports.PROTECT_GAME = false
 module.exports.SHUFFLE_QUESTIONS = true
